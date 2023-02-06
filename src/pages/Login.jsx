23f258d97164effef5f951ec43925bcb6e300540
Artur0705 <artlil420@gmail.com>
2023-02-06 20:32:49 +1100

    Sign up page UI has been created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,7 +38,9 @@ const Login = () => {
                 <div>
                   <Link to="/forgot-password">Forgot Password ?</Link>
                   <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
-                    <button className="button border-0">Login</button>
+                    <button className="button border-0" type="submit">
+                      Login
+                    </button>
                     <Link to="/signup" className="button signup">
                       Sign Up
                     </Link>

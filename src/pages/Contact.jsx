f44651d93774077f13a00d442cfa54c103df21aa
Artur0705 <artlil420@gmail.com>
2023-02-06 17:52:34 +1100

    Contact Page created contact us form styled

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,8 +5,72 @@ import Meta from "../components/Meta";
 const Contact = () => {
   return (
     <>
-      {/* <Meta title={"Contact Us"} />
-      <BreadCrumb title="Contact Us" /> */}
+      <Meta title={"Contact"} />
+      <BreadCrumb title="Contact" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                title="location"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12605.97142860411!2d144.95041075181456!3d-37.82534622690125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d450a8be10f%3A0x5045675218cfec0!2sSouthbank%20VIC%203006!5e0!3m2!1sen!2sau!4v1675665191823!5m2!1sen!2sau"
+                width="600"
+                height="450"
+                className="border-0 w-100"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get In Touch With Us</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

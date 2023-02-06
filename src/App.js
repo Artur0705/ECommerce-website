f54b95b1f87f3c38bdcdf55e931739944215fe41
Artur0705 <artlil420@gmail.com>
2023-02-06 19:54:36 +1100

    Login page UI has been created

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import OurStore from "./pages/OurStore";
 import Blog from "./pages/Blog";
 import CompareProduct from "./pages/CompareProduct";
 import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -23,6 +26,9 @@ function App() {
             <Route path="blogs" element={<Blog />} />
             <Route path="compare-product" element={<CompareProduct />} />
             <Route path="wishlist" element={<Wishlist />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route forgot-password="login" element={<ForgotPassword />} />
           </Route>
         </Routes>
       </BrowserRouter>

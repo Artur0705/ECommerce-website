286c3676326bc9eaf852dea9d391cd639fd01bf1
Artur0705 <artlil420@gmail.com>
2023-02-08 15:04:34 +1100

    Fixed horizontal scroll in Login, Sign up and Forgot password pages

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,66 +10,68 @@ const SignUp = () => {
       <BreadCrumb title="Sign Up" />
 
       <div className="login-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">Create an Account</h3>
-              <form
-                action="
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="auth-card">
+                <h3 className="text-center mb-3">Create an Account</h3>
+                <form
+                  action="
                     "
-                className="d-flex flex-column gap-15"
-              >
-                <div>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    className="form-control"
-                  />
-                </div>
-
-                <div>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    className="form-control"
-                  />
-                </div>
-
-                <div>
-                  <input
-                    type="tel"
-                    name="mobile"
-                    placeholder="Mobile Number"
-                    className="form-control"
-                  />
-                </div>
-                <div className="mt-1">
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    className="form-control"
-                  />
-                </div>
-                <div>
-                  <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
-                    <button className="button border-0">Sign Up</button>
+                  className="d-flex flex-column gap-15"
+                >
+                  <div>
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      className="form-control"
+                    />
                   </div>
-                  <div className="d-flex  justify-content-center align-items-center">
-                    <span to="/login" className="mt-3 d-flex flex-column">
-                      Already have an account ?{" "}
-                      <Link
-                        to="/login"
-                        className="d-flex justify-content-center align-items-center mt-3"
-                      >
-                        Sign In
-                      </Link>
-                    </span>
+
+                  <div>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      className="form-control"
+                    />
                   </div>
-                </div>
-              </form>
+
+                  <div>
+                    <input
+                      type="tel"
+                      name="mobile"
+                      placeholder="Mobile Number"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="mt-1">
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      className="form-control"
+                    />
+                  </div>
+                  <div>
+                    <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
+                      <button className="button border-0">Sign Up</button>
+                    </div>
+                    <div className="d-flex  justify-content-center align-items-center">
+                      <span to="/login" className="mt-3 d-flex flex-column">
+                        Already have an account ?{" "}
+                        <Link
+                          to="/login"
+                          className="d-flex justify-content-center align-items-center mt-3"
+                        >
+                          Sign In
+                        </Link>
+                      </span>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>

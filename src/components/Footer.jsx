7079a94a0fd56c0b82a515b374424d7f6d2de14c
Artur0705 <artlil420@gmail.com>
2023-02-08 15:49:00 +1100

    Policy pages have been created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -71,19 +71,19 @@ const Footer = () => {
             <div className="col-3">
               <h4 className="text-white mb-4">Information</h4>
               <div className="footer-links d-flex flex-column">
-                <Link to="/" className="text-white py-2 mb-1">
+                <Link to="/privacy-policy" className="text-white py-2 mb-1">
                   Privacy Policy
                 </Link>
-                <Link to="/" className="text-white py-2 mb-1">
+                <Link to="/refund-policy" className="text-white py-2 mb-1">
                   Refund Policy
                 </Link>
-                <Link to="/" className="text-white py-2 mb-1">
+                <Link to="/shipping-policy" className="text-white py-2 mb-1">
                   Shipping Policy
                 </Link>
-                <Link to="/" className="text-white py-2 mb-1">
+                <Link to="/terms-conditions" className="text-white py-2 mb-1">
                   Terms & Conditions
                 </Link>
-                <Link to="/" className="text-white py-2 mb-1">
+                <Link to="/blogs" className="text-white py-2 mb-1">
                   Blogs
                 </Link>
               </div>
@@ -97,7 +97,7 @@ const Footer = () => {
                 <Link to="/" className="text-white py-2 mb-1">
                   Faq
                 </Link>
-                <Link to="/" className="text-white py-2 mb-1">
+                <Link to="/contact" className="text-white py-2 mb-1">
                   Contact
                 </Link>
               </div>

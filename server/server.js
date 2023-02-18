8a387e4a5d9fe6202fe28576a0a75ed8891fb4c5
Artur0705 <artlil420@gmail.com>
2023-02-18 19:12:46 +1100

    Created route and controller for registering a user

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const db = require("./config/connection.js");
 const app = express();
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const authRouter = require("./routes/authRoute");
 const PORT = process.env.PORT || 8000;
 
-app.use("/", (req, res) => {
-  res.send("Test from server side");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/user", authRouter);
 
 db.once("open", () => {
   app.listen(PORT, () => {

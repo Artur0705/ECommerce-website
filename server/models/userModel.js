4199c5e5bd5fa812a443dd4e8f0d5077f879f6b5
Artur0705 <artlil420@gmail.com>
2023-02-18 19:54:03 +1100

    Used bcrypt to hash the user password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose"); // Erase if already required
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
 const userSchema = new mongoose.Schema({
@@ -25,6 +26,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.method.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 //Export the model
 module.exports = mongoose.model("User", userSchema);

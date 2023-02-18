f3a33009210adc94e4f995c0945b17000bbd7488
Artur0705 <artlil420@gmail.com>
2023-02-18 19:47:07 +1100

    Added error handling middleware and added express async handler package

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,17 +1,14 @@
 const User = require("../models/userModel");
-
-const createUser = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email: email });
   if (!findUser) {
     const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
-    res.json({
-      message: "User Already Exists",
-      succsess: false,
-    });
+    throw new Error("User Already Exists");
   }
-};
+});
 
 module.exports = { createUser };
--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ const app = express();
 const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
 const authRouter = require("./routes/authRoute");
+const { notFound, errorHandler } = require("./middleware/errorHandler.js");
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 db.once("open", () => {
   app.listen(PORT, () => {

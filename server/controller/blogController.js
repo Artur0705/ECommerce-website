dcf24f915b95edbf1efb5ff50e3ea8a12419a420
Artur0705 <artlil420@gmail.com>
2023-02-26 20:17:12 +1100

    Created update blog functionality and defined route

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -11,5 +11,16 @@ const createBlog = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
+const updateBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updateBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-module.exports = { createBlog };
+module.exports = { createBlog, updateBlog };

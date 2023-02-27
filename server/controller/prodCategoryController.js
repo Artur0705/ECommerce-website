3b9828aa66a3af7eac20f55a46720caeafcf1017
Artur0705 <artlil420@gmail.com>
2023-02-27 16:00:23 +1100

    Created get all categories controller and route

--- server/controller/prodCategoryController.js
+++ server/controller/prodCategoryController.js
@@ -47,9 +47,19 @@ const getCategory = asyncHandler(async (req, res) => {
   }
 });
 
+const getAllCategory = asyncHandler(async (req, res) => {
+  try {
+    const getAllCategory = await Category.find();
+    res.json(getAllCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createCategory,
   updateCategory,
   deleteCategory,
   getCategory,
+  getAllCategory,
 };

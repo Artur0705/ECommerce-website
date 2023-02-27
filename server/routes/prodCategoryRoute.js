d18f8b98a17dcbbe8461b71842f84d1328bae93b
Artur0705 <artlil420@gmail.com>
2023-02-27 15:53:40 +1100

    Created delete product category functionality and route

--- server/routes/prodCategoryRoute.js
+++ server/routes/prodCategoryRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createCategory,
   updateCategory,
+  deleteCategory,
 } = require("../controller/prodCategoryController");
 const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCategory);
 router.put("/:id", authMiddleware, isAdmin, updateCategory);
+router.delete("/:id", authMiddleware, isAdmin, deleteCategory);
 
 module.exports = router;

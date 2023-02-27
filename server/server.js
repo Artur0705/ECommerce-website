93ab6ae1d56f2e7610457f78a338b8e690552c11
Artur0705 <artlil420@gmail.com>
2023-02-27 16:12:14 +1100

    Created CRUD operations for Blog categories and created routes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const prodCategoryRouter = require("./routes/prodCategoryRoute");
+const blogCategoryRouter = require("./routes/blogCategoryRoute");
 const { notFound, errorHandler } = require("./middleware/errorHandler.js");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 8000;
@@ -21,6 +22,7 @@ app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/category", prodCategoryRouter);
+app.use("/api/blog-category", blogCategoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);

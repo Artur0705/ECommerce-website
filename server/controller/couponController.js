6d63355cb383a85d6d5f733fb3d8b23ef8e117eb
Artur0705 <artlil420@gmail.com>
2023-02-28 12:18:53 +1100

    Created create new coupon controller

---
+++ server/controller/couponController.js
@@ -0,0 +1,14 @@
+const Coupon = require("../models/couponModel");
+const { validate } = require("../models/couponModel");
+const asyncHandler = require("express-async-handler");
+
+const createCoupon = asyncHandler(async (req, res) => {
+  try {
+    const newCoupon = await Coupon.create(req.body);
+    res.json(newCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createCoupon };

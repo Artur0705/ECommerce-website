564ef997fbd103120be24b0413aa2288923a08d4
Artur0705 <artlil420@gmail.com>
2023-02-28 12:38:00 +1100

    Created get coupons route and controller

--- server/routes/couponRoute.js
+++ server/routes/couponRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCoupon } = require("../controller/couponController");
+const {
+  createCoupon,
+  getAllCoupons,
+} = require("../controller/couponController");
 const { isAdmin, authMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupons);
 
 module.exports = router;

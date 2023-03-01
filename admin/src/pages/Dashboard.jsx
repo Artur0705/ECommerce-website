c5c784814d4550eeec78113c22f7a2783f806101
Artur0705 <artlil420@gmail.com>
2023-03-01 21:22:17 +1100

    Created Dashboard UI upper part

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
+import { HiArrowTrendingDown, HiArrowTrendingUp } from "react-icons/hi2";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h3 className="mb-4">Dashboard</h3>
+      <div className="d-flex justify-content-between align-items-center gap-3">
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1899</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6>
+              <HiArrowTrendingUp />
+              29%
+            </h6>
+            <p className="mb-0">Compared to January 2023</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1899</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <HiArrowTrendingDown />
+              29%
+            </h6>
+            <p className="mb-0">Compared to January 2023</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1899</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <HiArrowTrendingUp />
+              29%
+            </h6>
+            <p className="mb-0">Compared to January 2023</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

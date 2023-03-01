60487a9ac7b37077681f06d3d96028a47cd2dce6
Artur0705 <artlil420@gmail.com>
2023-03-01 21:51:50 +1100

    Added charts and dummy data to dashboard using ant design charts and added table with dummy data using ant design, updated css

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,8 +1,120 @@
 import React from "react";
 
 import { HiArrowTrendingDown, HiArrowTrendingUp } from "react-icons/hi2";
+import { Column } from "@ant-design/plots";
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Order No.",
+    dataIndex: "key",
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Product",
+    dataIndex: "product",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+  },
+];
+const data1 = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    product: 32,
+    status: `London, Park Lane no. ${i}`,
+  });
+}
 
 const Dashboard = () => {
+  const data = [
+    {
+      type: "Jan",
+      sales: 38,
+    },
+    {
+      type: "Feb",
+      sales: 52,
+    },
+    {
+      type: "Mar",
+      sales: 61,
+    },
+    {
+      type: "Apr",
+      sales: 145,
+    },
+    {
+      type: "May",
+      sales: 48,
+    },
+    {
+      type: "June",
+      sales: 38,
+    },
+    {
+      type: "July",
+      sales: 38,
+    },
+    {
+      type: "Aug",
+      sales: 38,
+    },
+    {
+      type: "Sep",
+      sales: 38,
+    },
+    {
+      type: "Oct",
+      sales: 38,
+    },
+    {
+      type: "Nov",
+      sales: 38,
+    },
+    {
+      type: "Dec",
+      sales: 38,
+    },
+  ];
+  const config = {
+    data,
+    xField: "type",
+    yField: "sales",
+    color: ({ type }) => {
+      return "#ffd333";
+    },
+    label: {
+      // 可手动配置 label 数据标签位置
+      position: "middle",
+      // 'top', 'bottom', 'middle',
+      // 配置样式
+      style: {
+        fill: "#FFFFFF",
+        opacity: 1,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: "Month",
+      },
+      sales: {
+        alias: "Income",
+      },
+    },
+  };
   return (
     <div>
       <h3 className="mb-4">Dashboard</h3>
@@ -45,6 +157,18 @@ const Dashboard = () => {
             </h6>
             <p className="mb-0">Compared to January 2023</p>
           </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mb-4">Income Statistics</h3>
+        <div>
+          <Column {...config} />
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mb-4">Recent Orders</h3>
+        <div>
+          <Table columns={columns} dataSource={data1} />
         </div>
       </div>
     </div>

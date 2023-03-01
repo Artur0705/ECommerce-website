dabb6bb221c3172090083a0c3285ea2f0e0c92f4
Artur0705 <artlil420@gmail.com>
2023-03-01 18:19:50 +1100

    Created forgot password and password reset pages UI

--- admin/src/pages/ForgotPassword.jsx
+++ admin/src/pages/ForgotPassword.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import CustomInput from "../components/CustomInput";
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-50 bg-white rounded-3 mx-auto p-4">
+        <h3 className="text-center">Forgot Password</h3>
+        <p className="text-center">
+          Please enter your registered email address to reset your password
+        </p>
+        <form action="">
+          <CustomInput type="text" label="Email Address" id="email" />
+
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            type="submit"
+            style={{ background: "#ffd333" }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ForgotPassword;
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -13,8 +13,8 @@ const Login = () => {
         <h3 className="text-center">Login</h3>
         <p className="text-center">Login to your account to continue</p>
         <form action="">
-          <CustomInput type="text" placeholder="Email Address" id="email" />
-          <CustomInput type="password" placeholder="Password" id="pass" />
+          <CustomInput type="text" label="Email Address" id="email" />
+          <CustomInput type="password" label="Password" id="pass" />
           <button
             className="border-0 px-3 py-2 text-white fw-bold w-100"
             type="submit"

fc7842ac53305edae59c2d311e465292d2c5408e
Artur0705 <artlil420@gmail.com>
2023-03-01 20:12:13 +1100

    Added navigation links in Login page, updated sidebar UI and styled

--- admin/src/components/MainLayout.jsx
+++ admin/src/components/MainLayout.jsx
@@ -1,41 +1,148 @@
 import React, { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+  AiOutlineDashboard,
+  AiOutlineAppstoreAdd,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+
+import { CgUserList } from "react-icons/cg";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
+import { MdOutlineColorLens } from "react-icons/md";
+import { FaClipboardList, FaMicroblog } from "react-icons/fa";
+import { IoCreateOutline } from "react-icons/io5";
+import { VscRequestChanges } from "react-icons/vsc";
 import { Layout, Menu, theme } from "antd";
+
+import { useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
 const { Header, Sider, Content } = Layout;
+
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const navigate = useNavigate();
+
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3 mb-0">
+            <span className="sm-logo">iTT</span>
+            <span className="lg-logo">iThinkTech</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key === "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
             },
             {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              key: "customers",
+              icon: <CgUserList className="fs-4" />,
+              label: "Customers",
             },
             {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
+              key: "catalog",
+              icon: <AiOutlineAppstoreAdd className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineAppstoreAdd className="fs-4" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineUnorderedList className="fs-4" />,
+                  label: "Product-list",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "brand-list",
+                  icon: <AiOutlineUnorderedList className="fs-4" />,
+                  label: "Brand List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "category-list",
+                  icon: <AiOutlineUnorderedList className="fs-4" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <MdOutlineColorLens className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "color-list",
+                  icon: <AiOutlineUnorderedList className="fs-4" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blog",
+              icon: <FaMicroblog className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blog",
+                  icon: <IoCreateOutline className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blog-list",
+                  icon: <AiOutlineUnorderedList className="fs-4" />,
+                  label: "Blog List",
+                },
+                {
+                  key: "blog-category",
+                  icon: <IoCreateOutline className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <AiOutlineUnorderedList className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <VscRequestChanges className="fs-4" />,
+              label: "Enquiries",
             },
           ]}
         />
@@ -63,7 +170,7 @@ const MainLayout = () => {
             background: colorBgContainer,
           }}
         >
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>

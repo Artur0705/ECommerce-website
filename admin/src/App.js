892a11750192a4ad848452dcfd2dee0066daae35
Artur0705 <artlil420@gmail.com>
2023-03-02 17:25:40 +1100

    Reused antd table and data to create blog list, blog category list and enquiries pages

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,16 +5,22 @@ import Login from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
 import MainLayout from "./components/MainLayout";
+import Enquiries from "./pages/Enquiries";
+import BlogList from "./pages/BlogList";
+import BlogCategoryList from "./pages/BlogCategoryList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="enquiries" element={<Enquiries />} />
+          <Route path="blog-list" element={<BlogList />} />
+          <Route path="blog-category-list" element={<BlogCategoryList />} />
         </Route>
       </Routes>
     </Router>

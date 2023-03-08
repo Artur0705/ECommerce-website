ca9bd13ae21bb9e9e75d062aaa5f3390430728f5
Artur0705 <artlil420@gmail.com>
2023-03-08 16:14:26 +1100

    Created enquiry slice and service, added cases for loading, error,and success updated enquiry page and store to fetch the data added timestamps to enquiry model

--- admin/src/pages/Enquiries.jsx
+++ admin/src/pages/Enquiries.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { BiEdit } from "react-icons/bi";
+import { TiDeleteOutline } from "react-icons/ti";
+import { getEnquiries } from "../features/enquiry/enquirySlice";
 
 const columns = [
   {
-    title: "Order No.",
+    title: "Number",
     dataIndex: "key",
   },
   {
@@ -11,30 +16,71 @@ const columns = [
     dataIndex: "name",
   },
   {
-    title: "Product",
-    dataIndex: "product",
+    title: "Email",
+    dataIndex: "email",
+  },
+  {
+    title: "Mobile",
+    dataIndex: "mobile",
+  },
+  {
+    title: "Comment",
+    dataIndex: "comment",
+  },
+  {
+    title: "Date",
+    dataIndex: "date",
   },
   {
     title: "Status",
     dataIndex: "status",
   },
+  {
+    title: "Action",
+    dataIndex: "action",
+  },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Edward King ${i}`,
-    product: 32,
-    status: `London, Park Lane no. ${i}`,
-  });
-}
 
 const Enquiries = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getEnquiries());
+    // eslint-disable-next-line
+  }, []);
+  const enquiryState = useSelector((state) => state.enquiry.enquiries);
+
+  const data = [];
+  for (let i = 0; i < enquiryState.length; i++) {
+    data.push({
+      key: i + 1,
+      name: enquiryState[i].name,
+      email: enquiryState[i].email,
+      mobile: enquiryState[i].mobile,
+      comment: enquiryState[i].comment,
+      date: enquiryState[i].createdAt,
+      status: (
+        <>
+          <select name="" id="" className="form-control form-select">
+            <option value="">Set Status</option>
+          </select>
+        </>
+      ),
+
+      action: (
+        <>
+          <Link to="/" className="ms-3 text-danger fs-3">
+            <TiDeleteOutline />
+          </Link>
+        </>
+      ),
+    });
+  }
+
   return (
     <div>
       <h3 className="mb-4 title">Enquiries</h3>
       <div>
-        <Table columns={columns} dataSource={data1} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   );
--- server/models/enquiryModel.js
+++ server/models/enquiryModel.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 
 // Declare the Schema of the Mongo model
-var enquirySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+var enquirySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "Submitted",
+      enum: ["Submitted", "Received", "Contacted", "In Progress"],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "Submitted",
-    enum: ["Submitted", "Received", "Contacted", "In Progress"],
-  },
-});
+  { timestamps: true }
+);
 
 //Export the model
 module.exports = mongoose.model("Enquiry", enquirySchema);

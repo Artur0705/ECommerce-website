bd70e3920a3b01510bb90602e1fa2663b88fc2c0
Artur0705 <artlil420@gmail.com>
2023-03-08 14:26:17 +1100

    Created color slice and service, added cases for loading, error,and success updated color page and store to fetch the data

--- admin/src/pages/ColorList.jsx
+++ admin/src/pages/ColorList.jsx
@@ -1,40 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { BiEdit } from "react-icons/bi";
+import { TiDeleteOutline } from "react-icons/ti";
+import { getColors } from "../features/color/colorSlice";
 
 const columns = [
   {
-    title: "Order No.",
+    title: "Number",
     dataIndex: "key",
   },
   {
-    title: "Name",
-    dataIndex: "name",
+    title: "Title",
+    dataIndex: "title",
+    sorter: (a, b) => a.title.length - b.title.length,
   },
   {
-    title: "Product",
-    dataIndex: "product",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
+    title: "Action",
+    dataIndex: "action",
   },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Edward King ${i}`,
-    product: 32,
-    status: `London, Park Lane no. ${i}`,
-  });
-}
 
 const ColorList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getColors());
+    // eslint-disable-next-line
+  }, []);
+
+  const colorState = useSelector((state) => state.color.colors);
+
+  const data = [];
+  for (let i = 0; i < colorState.length; i++) {
+    data.push({
+      key: i + 1,
+      title: colorState[i].title,
+      action: (
+        <>
+          <Link to="/" className="text-success fs-3">
+            <BiEdit />
+          </Link>
+
+          <Link to="/" className="ms-3 text-danger fs-3">
+            <TiDeleteOutline />
+          </Link>
+        </>
+      ),
+    });
+  }
+
   return (
     <div>
       <h3 className="mb-4 title">Colors</h3>
       <div>
-        <Table columns={columns} dataSource={data1} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   );

5f6d9bd049b3c85bafdfef64efe8e5a719432fdc
Artur0705 <artlil420@gmail.com>
2023-03-15 15:58:42 +1100

    Updated add brand page to create brands

--- admin/src/pages/AddBrand.jsx
+++ admin/src/pages/AddBrand.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import CustomInput from "../components/CustomInput";
+import { createBrand } from "../features/brand/brandSlice";
+
+let schema = Yup.object().shape({
+  title: Yup.string().required("Brand Name is Required"),
+});
 
 const AddBrand = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const newBrand = useSelector((state) => state.brand);
+  const { isSuccess, isError, isLoading, createdBrand } = newBrand;
+
+  useEffect(() => {
+    if (isSuccess && createdBrand) {
+      toast.success("Brand Added Successfully!!!");
+    }
+    if (isError) {
+      toast.error("Something Went Wrong!");
+    } // eslint-disable-next-line
+  }, [isSuccess, isError, isLoading]);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(createBrand(values));
+      formik.resetForm();
+      setTimeout(() => {
+        navigate("/admin/brand-list");
+      }, 3000);
+    },
+  });
   return (
     <div>
       <h3 className="mb-4 title">Add Brand</h3>
       <div>
-        <form action="">
-          <CustomInput type="text" label="Enter Brand Name" />
+        <form action="" onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            label="Enter Brand Name"
+            name="title"
+            onChng={formik.handleChange("title")}
+            onBLr={formik.handleBlur("title")}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"

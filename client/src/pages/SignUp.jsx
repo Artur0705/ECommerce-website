f8e69ff2890f17404823ce65a9f0264424f5d305
Artur0705 <artlil420@gmail.com>
2023-04-07 18:54:36 +1000

    Added validation to sign up page using yup and formik packages

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,8 +4,33 @@ import BreadCrumb from "../components/BreadCrumb";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
 import Meta from "../components/Meta";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const signUpSchema = yup.object({
+  firstName: yup.string().required("First Name is Required"),
+  lastName: yup.string().required("Last Name is Required"),
+  email: yup.string().nullable().email("Email Should Be Valid"),
+  mobile: yup.number().required("Mobile Number is Required"),
+  password: yup.string().required("Password is required"),
+});
 
 const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      mobile: "",
+      password: "",
+    },
+
+    validationSchema: signUpSchema,
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <>
       <Meta title={"Sign Up"} />
@@ -17,22 +42,65 @@ const SignUp = () => {
             <div className="auth-card">
               <h3 className="text-center mb-3">Create an Account</h3>
               <form
-                action="
-                    "
+                action=" "
+                onSubmit={formik.handleSubmit}
                 className="d-flex flex-column gap-15"
               >
-                <CustomInput type="text" name="name" placeholder="Name" />
-                <CustomInput type="email" name="email" placeholder="Email" />
+                <CustomInput
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange("firstName")}
+                  onBlur={formik.handleBlur("firstName")}
+                />
+                <div className="error">
+                  {formik.touched.firstName && formik.errors.firstName}
+                </div>
+                <CustomInput
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange("lastName")}
+                  onBlur={formik.handleBlur("lastName")}
+                />
+                <div className="error">
+                  {formik.touched.lastName && formik.errors.lastName}
+                </div>
+                <CustomInput
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange("email")}
+                  onBlur={formik.handleBlur("email")}
+                />
+                <div className="error">
+                  {formik.touched.email && formik.errors.email}
+                </div>
                 <CustomInput
                   type="tel"
                   name="mobile"
                   placeholder="Mobile Number"
+                  value={formik.values.mobile}
+                  onChange={formik.handleChange("mobile")}
+                  onBlur={formik.handleBlur("mobile")}
                 />
+                <div className="error">
+                  {formik.touched.mobile && formik.errors.mobile}
+                </div>
                 <CustomInput
                   type="password"
                   name="password"
                   placeholder="Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange("password")}
+                  onBlur={formik.handleBlur("password")}
                 />
+                <div className="error">
+                  {formik.touched.password && formik.errors.password}
+                </div>
 
                 <div>
                   <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
